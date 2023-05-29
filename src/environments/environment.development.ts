8455336f4e7df1ac9557af8b4fe9bae9bd931eb3
Alef Santos Soares <54686408+Olafi-MooN@users.noreply.github.com>
2023-05-29 11:01:02 -0300

    remove environments

--- src/environments/environment.development.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://sinple-api-dev.us-east-1.elasticbeanstalk.com:8080',
-  oauth2AccessTokenUrl:
-    'http://auth-keycloak-dev.us-east-1.elasticbeanstalk.com/realms/princeton-lemitar/protocol/openid-connect/token',
-  oauth2ClientId: 'sinple-web',
-  oauth2ClientSecret: 'ZzVCevKWN9kQ1SNjahS6HhQ6yB4bqdc6',
-};
